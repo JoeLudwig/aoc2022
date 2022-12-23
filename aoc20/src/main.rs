894f3268eaf0bed7bc1c734140e07e7d011e16c4
Joe Ludwig <joe@valvesoftware.com>
2022-12-23 14:23:22 -0800

    Part 2

--- aoc20/src/main.rs
+++ aoc20/src/main.rs
@@ -10,7 +10,7 @@ use std::collections::VecDeque;
 struct Num
 {
 	id: usize,
-	number: i32,
+	number: i64,
 }
 
 
@@ -35,93 +35,86 @@ fn main()
 	
 	let mut numbers: Vec< Num > = Vec::new();
 
+	let mult = 811589153;
+	let mix_count = 10;
 	while let Some( line ) = lines.next()
 	{
 		let cur_line = line.unwrap();
 
-		numbers.push( Num{ id: numbers.len(), number: cur_line.parse::<i32>().unwrap() } );
+		numbers.push( Num{ id: numbers.len(), number: mult * cur_line.parse::<i64>().unwrap() } );
 	}
 
 	dump_numbers( &numbers );
 	println!("");
 
-	let len = numbers.len() as i32;
-	for i in 0..numbers.len()
+	for _ in 0..mix_count
 	{
-		// find the number that matches i, wherever it currently is
-		let mut ind = 0;
-		for test in 0..numbers.len()
+		let len = numbers.len() as i64;
+		for i in 0..numbers.len()
 		{
-			if numbers[test].id == i
+			// find the number that matches i, wherever it currently is
+			let mut ind = 0;
+			for test in 0..numbers.len()
 			{
-				ind = test;
-				break;
-			}
-		}
-
-		if numbers[ ind ].number != 0
-		{
-			let to_move = numbers[ind].number;
-			let mut insert_after = ind as i32 + to_move;
-
-			println!( "");
-			if insert_after < 0
-			{
-				insert_after -= 1;
-				let n = insert_after.abs() / len;
-				println!( "insert_after={}, n={}", insert_after, n );
-				insert_after += ( n + 1 ) * len;
-			}
-
-			insert_after = insert_after % len;
-			/*if insert_after > ind as i32
-			{
-				// I'll remove the thing at ind first, which implicitly increases 
-				// insert_after by one. So subtract that one back out
-				insert_after -= 1;
-			} */
-			//insert_after = if insert_after <= ind as i32 { insert_after } else { insert_after - 1 };
-			println!( "i={}, ind={}, insert_after={}", i, ind, insert_after );
-			let prev = ( (insert_after + len ) % len ) as usize;
-			let next = ( ( (insert_after + 1 ) % len ) as usize ) % numbers.len();
-			println!( "{} moves between {} and {}", to_move,
-		 		numbers[ prev ].number,
-		 		numbers[ next ].number,
-			);
-			/*
-			*/
-
-			if insert_after as usize == numbers.len() || insert_after == 0
-			{
-				let moving = numbers.remove( ind );
-				numbers.push( moving );
-			}
-			else
-			{
-				if insert_after > ind as i32
+				if numbers[test].id == i
 				{
-					// we're trying to insert after a slot, but removing moves that slot to the left, 
-					// so the insert call below (which is insert before) is essentially insert after
-					let moving = numbers.remove( ind );
-					numbers.insert( insert_after as usize, moving );
+					ind = test;
+					break;
+				}
+			}
+
+			if numbers[ ind ].number != 0
+			{
+				let to_move = numbers[ind].number;
+				let moving = numbers.remove( ind );
+				let mut insert_before = ind as i64;
+
+				//println!( "");
+
+				// first add how many positions it wants to move
+				insert_before += to_move;
+
+				// then make it positive if it isn't
+				let len = numbers.len() as i64;
+				if insert_before < 0
+				{
+					let n = insert_before.abs() / len;
+					//println!( "insert_before={}, n={}", insert_before, n );
+					insert_before += ( n + 1 ) * len;
+				}
+
+				// then make it in the right range
+				insert_before = insert_before % len;
+
+				/*
+				println!( "i={}, ind={}, insert_before={}", i, ind, insert_before );
+				let prev = ( (insert_before + len  - 1 ) % len ) as usize;
+				let next = insert_before as usize;
+				println!( "{} moves between {} and {}", to_move,
+					numbers[ prev ].number,
+					numbers[ next ].number,
+				);
+				*/
+
+				if insert_before == 0
+				{
+					numbers.push( moving );
 				}
 				else
 				{
-
-					let moving = numbers.remove( ind );
-					numbers.insert( insert_after as usize + 1, moving );
+					numbers.insert( insert_before as usize, moving );
 				}
+				//dump_numbers( &numbers );
 			}
-			dump_numbers( &numbers );
+			else
+			{
+				//println!( "\n0 does not move" );
+				//dump_numbers( &numbers );
+			}
 		}
-		else
-		{
-			println!( "\n0 does not move" );
-			dump_numbers( &numbers );
-		}
+		dump_numbers( &numbers );
 	}
 
-	dump_numbers( &numbers );
 
 	let mut ind = 0;
 	for test in 0..numbers.len()
