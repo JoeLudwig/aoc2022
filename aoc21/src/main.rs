8e295f5e8df75260244ace08c903fbc7211e11fa
Joe Ludwig <joe@valvesoftware.com>
2022-12-24 11:23:05 -0800

    day 21 part 2

--- aoc21/src/main.rs
+++ aoc21/src/main.rs
@@ -18,7 +18,18 @@ enum Op
 	Human,
 }
 
-fn match_pair( op: Op ) -> ( String, String )
+#[derive(Clone,Copy,Debug)]
+enum SimpleOp 
+{
+	Add,
+	Subtract,
+	SubtractReverse,
+	Multiply,
+	Divide,
+	DivideReverse,
+}
+
+fn match_pair( op: &Op ) -> ( String, String )
 {
 	
 	match op
@@ -29,7 +40,7 @@ fn match_pair( op: Op ) -> ( String, String )
 		| Op::Divide( a, b ) 
 		=> 
 		{ 
-			return ( a, b );
+			return ( a.clone(), b.clone() );
 		}
 		_ => panic!( "match_pair can only be called on add/subtract/multiply/divide ops: {:?}", op ),
 	}
@@ -39,24 +50,19 @@ fn run_op( monkey_name: &String, monkeys: &HashMap< String, Op > ) -> Option< i6
 {
 	let op = monkeys.get( monkey_name ).unwrap();
 
-	let mut a_value: Option< i64 >;
-	let mut b_value: Option< i64 >;
+	let a_value: Option< i64 >;
+	let b_value: Option< i64 >;
 
 	match op
 	{
 		Op::Human => return None,
 		Op::Number( n ) => return Some( *n ),
-		Op::Add( a, b ) 
-		| Op::Subtract( a, b ) 
-		| Op::Multiply( a, b )
-		| Op::Divide( a, b ) 
-		=> 
-		{ 
-			a_value = run_op( a, monkeys );
-			b_value = run_op( b, monkeys );
-		}
+		_ => {},
 	}
 	
+	let ( a, b ) = match_pair( op );
+	a_value = run_op( &a, monkeys );
+	b_value = run_op( &b, monkeys );
 	if a_value == Option::None || b_value == Option::None
 	{
 		return Option::None;
@@ -75,6 +81,62 @@ fn run_op( monkey_name: &String, monkeys: &HashMap< String, Op > ) -> Option< i6
 	};
 }
 
+fn compute_unknown_value( result: i64, known: i64, op: SimpleOp ) -> i64
+{
+	return match op
+	{
+		SimpleOp::Add => result - known, 			// result = known + human
+		SimpleOp::Subtract => known - result, 		// result = known - human
+		SimpleOp::SubtractReverse => result + known,// result = human - known
+		SimpleOp::Multiply => result / known, 		// result = known * human
+		SimpleOp::Divide => known / result,			// result = known / human
+		SimpleOp::DivideReverse => known * result, 	// result = human / known
+	};
+}
+
+
+fn make_human_equal( result: i64, known: i64, op: SimpleOp, human_branch: &String, monkeys: &HashMap< String, Op > ) -> i64
+{
+	let unknown_value = compute_unknown_value( result, known, op );
+	let child_op = monkeys.get( human_branch ).unwrap();
+	
+	println!( "Determining {} = {} {:?} {} ( {} ) for {:?}",
+		result, known, op, human_branch, unknown_value, child_op );
+	let simple_op: SimpleOp = match child_op
+	{
+		Op::Human =>
+		{
+			return unknown_value;
+		},
+		Op::Number(_) => panic!( "A simple number monkey shouldn't be on the unresolved side" ),
+		Op::Add(_,_) => SimpleOp::Add,
+		Op::Subtract(_,_) => SimpleOp::Subtract,
+		Op::Multiply(_,_) => SimpleOp::Multiply,
+		Op::Divide(_,_) => SimpleOp::Divide,
+	};
+
+	let ( a, b ) = match_pair( child_op );
+
+	let a_value = run_op( &a, &monkeys );
+	let b_value = run_op( &b, &monkeys );
+
+	return if a_value == Option::None
+	{
+		let simple_op = match simple_op
+		{
+			SimpleOp::Subtract => SimpleOp::SubtractReverse,
+			SimpleOp::Divide => SimpleOp::DivideReverse,
+			_ => simple_op,
+		};
+
+		make_human_equal( unknown_value, b_value.unwrap(), simple_op, &a, monkeys )
+	}
+	else
+	{
+		make_human_equal( unknown_value, a_value.unwrap(), simple_op, &b, monkeys )
+	};
+}
+
 
 fn main()
 {
@@ -90,10 +152,17 @@ fn main()
 		let ( monkey_name, details ) = cur_line.split_once( ": " ).unwrap();
 
 		let args: Vec<&str> = details.split( ' ' ).collect();
-		println!( "monkey_name = {}, details={}, args={:?}", monkey_name, details, args );
+		//println!( "monkey_name = {}, details={}, args={:?}", monkey_name, details, args );
 		let op = if args.len() == 1
 		{
-			Op::Number( args[0].parse::<i64>().unwrap() )
+			if monkey_name == "humn"
+			{
+				Op::Human
+			}
+			else
+			{
+				Op::Number( args[0].parse::<i64>().unwrap() )
+			}
 		}
 		else
 		{
@@ -113,10 +182,23 @@ fn main()
 		monkeys.insert( monkey_name.to_string(), op );
 	}
 
-	println!( "root: {:?}", run_op( &"root".to_string(), &monkeys ) );
+//	println!( "root: {:?}", run_op( &"root".to_string(), &monkeys ) );
 
-	//let root_op = monkeys.get( &"root".to_string(), &monkeys ).unwrap();
+	let root_op = monkeys.get( &"root".to_string() ).unwrap();
+	let ( a, b ) = match_pair( root_op );
 
+	let a_value = run_op( &a, &monkeys );
+	let b_value = run_op( &b, &monkeys );
 
+	let human_value = if a_value == Option::None
+	{
+		make_human_equal( b_value.unwrap(), 0, SimpleOp::Add, &a, &monkeys )
+	}
+	else
+	{
+		make_human_equal( a_value.unwrap(), 0, SimpleOp::Add, &b, &monkeys )
+	};
+
+	println!( "Human value: {}", human_value );
 }
 
